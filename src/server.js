55ffde4137e58cec8988e12046179b5e8b2376f3
victoriaUNI <victoria.00000851663@unicap.br>
2025-06-07 19:43:46 +0000

    .serevr.js deletado

--- src/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
